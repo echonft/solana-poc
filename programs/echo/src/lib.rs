bd804fdde6f8dbb7ef13ce8d1dd255dacccfb2c8
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-04-09 21:43:56 -0400

    switched to transfer

--- programs/echo/src/lib.rs
+++ programs/echo/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_spl::associated_token::{AssociatedToken, ID as SPL_ASSOCIATED_TOKEN_PROGRAM_ID};
 use anchor_spl::token::spl_token::{instruction::AuthorityType, ID as SPL_TOKEN_PROGRAM_ID};
-use anchor_spl::token::{set_authority, Mint, SetAuthority, Token, TokenAccount};
+use anchor_spl::token::{transfer, Mint, Token, TokenAccount, Transfer};
 
 declare_id!("Gzq13nAmkDZMgFjKs8Zd6jJbXE2X6iJJ4FEe2BqWcVWM");
 
@@ -11,70 +12,77 @@ pub mod echo {
         Ok(())
     }
     pub fn create_offer(ctx: Context<CreateOfferAccounts>) -> Result<()> {
-        set_authority(
-            CpiContext::new(
-                ctx.accounts.spl_token_program.to_account_info(),
-                SetAuthority {
-                    account_or_mint: ctx.accounts.token.to_account_info(),
-                    current_authority: ctx.accounts.sender.to_account_info(),
-                },
-            ),
-            AuthorityType::AccountOwner,
-            Some(ctx.accounts.escrow.key()),
-        )
-        .unwrap();
-
         let offer = &mut ctx.accounts.offer;
         offer.sender = ctx.accounts.sender.key();
         offer.receiver = ctx.accounts.receiver.key();
-        offer.sender_token = ctx.accounts.token.key();
-        offer.receiver_token = ctx.accounts.receiver_token.key();
+        offer.sender_token_mint = ctx.accounts.token_mint.key();
+        offer.receiver_token_mint = ctx.accounts.receiver_token_mint.key();
         offer.state = OfferState::OPEN;
         offer.bump = ctx.bumps.offer;
+        msg!("offer data set");
 
         let escrow = &mut ctx.accounts.escrow;
         escrow.owner = ctx.accounts.sender.key();
-        escrow.token = ctx.accounts.token.key();
+        escrow.mint = ctx.accounts.token_mint.key();
         escrow.offer = ctx.accounts.offer.key();
         escrow.bump = ctx.bumps.escrow;
+        msg!("escrow data set");
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.spl_token_program.to_account_info(),
+                Transfer {
+                    authority: ctx.accounts.sender.to_account_info(),
+                    from: ctx.accounts.token.to_account_info(),
+                    to: ctx
+                        .accounts
+                        .escrow_associated_token_account
+                        .to_account_info(),
+                },
+            ),
+            1,
+        )
+        .unwrap();
+        msg!("Tokens escrowed, saving offer data");
 
         Ok(())
     }
 
     pub fn accept_offer(ctx: Context<AcceptOfferAccounts>) -> Result<()> {
         // TODO assert offer is not expired
+        // FIXME use transfer
         // transfer the token from sender escrow -> receiver
-        set_authority(
-            CpiContext::new_with_signer(
-                ctx.accounts.spl_token_program.to_account_info(),
-                SetAuthority {
-                    account_or_mint: ctx.accounts.sender_token.to_account_info(),
-                    current_authority: ctx.accounts.sender_escrow.to_account_info(),
-                },
-                &[&[
-                    b"escrow",
-                    ctx.accounts.sender.key().as_ref(),
-                    ctx.accounts.sender_token.key().as_ref(),
-                    &[ctx.accounts.sender_escrow.bump],
-                ]],
-            ),
-            AuthorityType::AccountOwner,
-            Some(ctx.accounts.receiver.key()),
-        )
-        .unwrap();
-        // transfer the token from receiver -> sender
-        set_authority(
-            CpiContext::new(
-                ctx.accounts.spl_token_program.to_account_info(),
-                SetAuthority {
-                    account_or_mint: ctx.accounts.sender_token.to_account_info(),
-                    current_authority: ctx.accounts.sender_escrow.to_account_info(),
-                },
-            ),
-            AuthorityType::AccountOwner,
-            Some(ctx.accounts.sender.key()),
-        )
-        .unwrap();
+        // set_authority(
+        //     CpiContext::new_with_signer(
+        //         ctx.accounts.spl_token_program.to_account_info(),
+        //         SetAuthority {
+        //             account_or_mint: ctx.accounts.sender_token_mint.to_account_info(),
+        //             current_authority: ctx.accounts.sender_escrow.to_account_info(),
+        //         },
+        //         &[&[
+        //             b"escrow",
+        //             ctx.accounts.sender.key().as_ref(),
+        //             ctx.accounts.sender_token_mint.key().as_ref(),
+        //             &[ctx.accounts.sender_escrow.bump],
+        //         ]],
+        //     ),
+        //     AuthorityType::AccountOwner,
+        //     Some(ctx.accounts.receiver.key()),
+        // )
+        // .unwrap();
+        // // transfer the token from receiver -> sender
+        // set_authority(
+        //     CpiContext::new(
+        //         ctx.accounts.spl_token_program.to_account_info(),
+        //         SetAuthority {
+        //             account_or_mint: ctx.accounts.token_mint.to_account_info(),
+        //             current_authority: ctx.accounts.receiver.to_account_info(),
+        //         },
+        //     ),
+        //     AuthorityType::AccountOwner,
+        //     Some(ctx.accounts.sender.key()),
+        // )
+        // .unwrap();
 
         Ok(())
     }
@@ -88,9 +96,9 @@ pub enum OfferState {
 }
 
 #[account]
-pub struct Escrow {
+pub struct EscrowAuthority {
     owner: Pubkey,
-    token: Pubkey,
+    mint: Pubkey,
     offer: Pubkey,
     bump: u8,
 }
@@ -99,8 +107,8 @@ pub struct Escrow {
 pub struct Offer {
     sender: Pubkey,
     receiver: Pubkey,
-    sender_token: Pubkey,
-    receiver_token: Pubkey,
+    sender_token_mint: Pubkey,
+    receiver_token_mint: Pubkey,
     state: OfferState,
     bump: u8,
 }
@@ -112,7 +120,7 @@ pub struct Initialize {}
 pub struct CreateOfferAccounts<'info> {
     #[account(mut)]
     pub sender: Signer<'info>,
-    #[account(
+    #[account(mut,
     token::mint = token_mint,
     token::authority = sender
     )]
@@ -122,21 +130,27 @@ pub struct CreateOfferAccounts<'info> {
     init,
     payer = sender,
     space = 8 + 32 + 32 + 32 + 1,
-    seeds = [b"escrow", sender.key().as_ref(), token.key().as_ref()],
+    seeds = [b"escrow", sender.key().as_ref(), token_mint.key().as_ref()],
     bump
     )]
-    pub escrow: Account<'info, Escrow>,
+    pub escrow: Account<'info, EscrowAuthority>,
+    #[account(init,
+    payer = sender,
+    associated_token::mint = token_mint,
+    associated_token::authority = escrow,
+    associated_token::token_program = associated_token_program
+    )]
+    pub escrow_associated_token_account: Account<'info, TokenAccount>,
     // TODO get a better seed: with a nonce (possibly # of offers created?)
     #[account(
     init,
     payer = sender,
     space = 8 + 32 + 32 + 32 + 32 + 1 + 1,
-    seeds = [b"offer", sender.key().as_ref(), token.key().as_ref()],
+    seeds = [b"offer", sender.key().as_ref(), token_mint.key().as_ref()],
     bump
     )]
     pub offer: Account<'info, Offer>,
-    /// CHECK: This is not dangerous because we're checking the address from the offer
-    #[account(address = offer.receiver.key())]
+    /// CHECK: This is not dangerous because we're not writing or reading from this account
     pub receiver: UncheckedAccount<'info>,
     #[account(
     token::mint = receiver_token_mint,
@@ -146,6 +160,8 @@ pub struct CreateOfferAccounts<'info> {
     pub receiver_token_mint: Account<'info, Mint>,
     #[account(address = SPL_TOKEN_PROGRAM_ID)]
     pub spl_token_program: Program<'info, Token>,
+    #[account(address = SPL_ASSOCIATED_TOKEN_PROGRAM_ID)]
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
 }
 
@@ -162,7 +178,7 @@ pub struct AcceptOfferAccounts<'info> {
     // TODO get a better seed: with a nonce (possibly # of offers created?)
     #[account(mut,
     close = sender,
-    seeds = [b"offer", sender.key().as_ref(), sender_token.key().as_ref()],
+    seeds = [b"offer", sender.key().as_ref(), sender_token_mint.key().as_ref()],
     bump
     )]
     pub offer: Account<'info, Offer>,
@@ -172,15 +188,16 @@ pub struct AcceptOfferAccounts<'info> {
     pub sender: UncheckedAccount<'info>,
     #[account(mut,
     close = sender,
-    seeds = [b"escrow", sender.key().as_ref(), sender_token.key().as_ref()],
+    seeds = [b"escrow", sender.key().as_ref(), sender_token_mint.key().as_ref()],
     bump
     )]
-    pub sender_escrow: Account<'info, Escrow>,
-    #[account(
-    token::mint = sender_token_mint,
-    token::authority = sender_escrow
+    pub sender_escrow: Account<'info, EscrowAuthority>,
+    #[account(mut,
+    close = sender,
+    associated_token::mint = sender_token_mint,
+    associated_token::authority = sender_escrow
     )]
-    pub sender_token: Account<'info, TokenAccount>,
+    pub sender_escrow_token_account: Account<'info, EscrowAuthority>,
     pub sender_token_mint: Account<'info, Mint>,
     #[account(address = SPL_TOKEN_PROGRAM_ID)]
     pub spl_token_program: Program<'info, Token>,
